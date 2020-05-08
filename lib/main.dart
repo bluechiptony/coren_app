6b4122bb395f722afddc66e31fdd5c3dee9a50df
bluechiptony <tony5egwu@gmail.com>
2020-05-08 15:46:08 +0100

    Scaffold App removed with stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Center(
-        child: Text("Howdy"),
-      ),
-    ),
-  );
+  runApp(CorenApp());
 }
+
+class CorenApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+//        appBar: AppBar(
+//          backgroundColor: Colors.green[900],
+//          title: Text("Coren"),
+//        ),
+        backgroundColor: Colors.green[900],
+        body: Center(
+          child: Image(
+            image: AssetImage('images/coren-logo.jpg'),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyContainer extends Scaffold {}
