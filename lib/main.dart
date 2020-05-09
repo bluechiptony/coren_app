8cf81cda2bf19f255fd7e47c2c5acb180904397d
bluechiptony <tony5egwu@gmail.com>
2020-05-09 22:16:29 +0100

    Card added to container for login screen

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class CorenApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(fontFamily: 'Montserrat'),
       home: Scaffold(
 //        appBar: AppBar(
 //          backgroundColor: Colors.green[900],
@@ -16,45 +17,53 @@ class CorenApp extends StatelessWidget {
         backgroundColor: Colors.green[900],
         body: SafeArea(
           child: Center(
-            child: Container(
-              color: Colors.white,
+            child: Card(
               margin: EdgeInsets.all(20.0),
-              padding: EdgeInsets.all(20.0),
-              child: Form(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    Text("Please enter your Login details"),
-                    SizedBox(
-                      height: 50,
-                    ),
-                    TextField(
-                      decoration: InputDecoration(
-                          labelText: "Email addresss",
-                          border: OutlineInputBorder()),
-                    ),
-                    SizedBox(
-                      height: 20.0,
-                    ),
-                    TextField(
-                      decoration: InputDecoration(
-                          hintText: "Secret password",
-                          labelText: "Password",
-                          border: OutlineInputBorder()),
-                    ),
-                    SizedBox(
-                      height: 20.0,
-                    ),
-                    RaisedButton(
-                      color: Colors.blue,
-                      textColor: Colors.white,
-                      disabledColor: Colors.green[900],
-                      disabledTextColor: Colors.white,
-                      padding: EdgeInsets.fromLTRB(50, 20, 50, 20),
-                      splashColor: Colors.blueAccent,
-                      child: Text("Login"),
-                    )
-                  ],
+              color: Colors.white,
+              child: Container(
+                padding: EdgeInsets.all(20.0),
+                child: Form(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Image(
+                        image: AssetImage("images/coren-logo.jpg"),
+                        height: 30,
+                      ),
+                      SizedBox(height: 20.0),
+                      Text("Please enter your Login details"),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                            labelText: "Email addresss",
+                            border: OutlineInputBorder()),
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                            hintText: "Secret password",
+                            labelText: "Password",
+                            border: OutlineInputBorder()),
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      RaisedButton(
+                        color: Colors.blue,
+                        textColor: Colors.white,
+                        disabledColor: Colors.green[900],
+                        disabledTextColor: Colors.white,
+                        padding: EdgeInsets.fromLTRB(50, 20, 50, 20),
+                        splashColor: Colors.blueAccent,
+                        child: Text("Login"),
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
